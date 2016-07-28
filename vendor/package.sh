911786fd66ecb7aa01874d237b9cc2aaaad992a8
Jeff Yutzler <jyutzler@yahoo.com>
2016-07-28 15:50:26 -0400

    removing unnecessary stuff from buildpack

--- vendor/package.sh
+++ vendor/package.sh
@@ -1,25 +1,25 @@
 sudo apt-get update
 sudo apt-get install -y libgeos-dev
-wget https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xvf go1.5.3.linux-amd64.tar.gz
-export GOROOT=/usr/local/go
-export PATH=$GOROOT/bin:$PATH
-export GOPATH=~/go
-sudo apt-get install -y git
-mkdir go
-cd go
-mkdir src
-cd src
-mkdir github.com
-cd github.com
-mkdir venicegeo
-cd venicegeo
-git clone https://github.com/venicegeo/pzsvc-image-catalog.git
-cd pzsvc-image-catalog
-go get ./...
-go build
+#wget https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz
+#sudo tar -C /usr/local -xvf go1.5.3.linux-amd64.tar.gz
+#export GOROOT=/usr/local/go
+#export PATH=$GOROOT/bin:$PATH
+#export GOPATH=~/go
+#sudo apt-get install -y git
+#mkdir go
+#cd go
+#mkdir src
+#cd src
+#mkdir github.com
+#cd github.com
+#mkdir venicegeo
+#cd venicegeo
+#git clone https://github.com/venicegeo/pzsvc-image-catalog.git
+#cd pzsvc-image-catalog
+#go get ./...
+#go build
 mkdir lib
 cp /usr/lib/libgeos* lib
-tar -zcvf pzsvc-image-catalog.tar.gz pzsvc-image-catalog lib
-mv pzsvc-image-catalog.tar.gz /vagrant
+tar -zcvf geos.tar.gz lib
+mv geos.tar.gz /vagrant
 
